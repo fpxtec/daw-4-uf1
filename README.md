394412f5bbeed539359ff25fd0608679cb6d0c0f
David de Mingo <david@optersoft.com>
2022-07-12 14:31:02 +0200

    jetty server runs

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # daw-4-uf1
 
+
+https://mkyong.com/webservices/jax-rs/jersey-and-jetty-http-server-examples/
+
 https://www.vogella.com/tutorials/REST/article.html
 https://github.com/christierney/kotlin-jetty-jersey-example/blob/master/build.gradle.kts
+
+
 
 https://docs.gradle.org/7.4.2/userguide/multi_project_builds.html
 
@@ -12,4 +17,3 @@ $ gradle -q run
 
 vscode 
 
-https://github.com/fwcd/vscode-kotlin/issues/33
--- app/src/main/java/daw4/uf1/App.java
+++ app/src/main/java/daw4/uf1/App.java
@@ -1,10 +1,17 @@
 package daw4.uf1;
 
+import java.net.URI;
+
+/* 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+*/
+
+import org.eclipse.jetty.server.Server;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
 public class App {
     public String getGreeting() {
@@ -13,16 +20,29 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        final ResourceConfig config = new ResourceConfig().packages("daw4.uf1");
+        final Server server = JettyHttpContainerFactory.createServer(
+                URI.create("http://localhost:8080/"), config);
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
-@Path("/CurrencyService")
-class CurrencyResource {
-
-    @GET
-    @Path("/hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String helloWorld() {
-        return "Hello!";
-    }
-}
+/*
+ * @Path("/CurrencyService")
+ * class CurrencyResource {
+ * 
+ * @GET
+ * 
+ * @Path("/hello")
+ * 
+ * @Produces(MediaType.TEXT_PLAIN)
+ * public String helloWorld() {
+ * return "Hello!";
+ * }
+ * }
+ */
